59c83145c71d6be7dcc37ee4b1acdb7a3c9c3629
user.name <aokblast@FreeBSD.org>
2024-04-07 16:22:04 +0800

    refactor: use cargo-fmt to refactor and use uppercase to file format

--- src/assignment.rs
+++ src/assignment.rs
@@ -1,201 +1,223 @@
-use std::{fs::DirEntry, path::Path, io, alloc::System, process::{Command, Stdio}};
-
-use fuzzy_matcher::{skim::SkimMatcherV2, FuzzyMatcher};
-
-use crate::Record;
+use std::{
+    io,
+    path::{Path, PathBuf},
+    process::{Command, Stdio},
+};
 
 #[derive(Clone, Copy, Debug)]
 pub enum ProgramType {
-	C
+    C,
 }
 
 impl ProgramType {
-	fn get_suffixes(self) -> &'static [(&'static str, &'static str)] {
-		match self {
-			Self::C => &[("c", "cc")]
-		}
-	}
+    fn get_suffixes(self) -> &'static [(&'static str, &'static str)] {
+        match self {
+            Self::C => &[("c", "cc")],
+        }
+    }
 
-	fn grade(self, file_name: &str, dir_name: &str, prog_name: &str) -> Option<u32> {
-		let output_file = format!("{dir_name}/output");
-		match self {
-			ProgramType::C => {
-				Command::new(prog_name).arg(file_name).arg(&format!("-o '{output_file}'")).spawn().unwrap();
-				Command::new(output_file).spawn().unwrap();
-			}
-		}
+    fn grade(self, file_name: &str, dir_name: &str, prog_name: &str) -> Option<u32> {
+        let output_file = format!("{dir_name}/output");
+        match self {
+            ProgramType::C => {
+                Command::new(prog_name)
+                    .arg(file_name)
+                    .arg(&format!("-o '{output_file}'"))
+                    .spawn()
+                    .unwrap();
+                Command::new(output_file).spawn().unwrap();
+            }
+        }
 
-		let mut line = String::new();
-		io::stdin().read_line(&mut line).unwrap();
-		Some(line.trim().parse().unwrap())
-	}
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
+        Some(line.trim().parse().unwrap())
+    }
 }
 
 #[derive(Clone, Copy, Debug)]
 pub enum FileType {
-	DOC,
-	PROGRAM(ProgramType, bool), // (Which type of program, raw file?)
-	PIC
+    Doc,
+    Program(ProgramType, bool), // (Which type of program, raw file?)
+    Pic,
 }
 
 impl FileType {
-	// Get the (file_extension, command) pair of a program
-	fn get_suffixes(self) -> &'static [(&'static str, &'static str)] {
-		match self {
-			FileType::DOC => {
-				&[("txt", "kate"), ("pdf", "evince")]
-			}
-			FileType::PIC => {
-				&[("jpg", "feh"), ("png", "feh")]
-			}
-			FileType::PROGRAM(program, _) => {
-				program.get_suffixes()
-			}
-		}
-	}
+    // Get the (file_extension, command) pair of a program
+    fn get_suffixes(self) -> &'static [(&'static str, &'static str)] {
+        match self {
+            FileType::Doc => &[("txt", "kate"), ("pdf", "evince")],
+            FileType::Pic => &[("jpg", "feh"), ("png", "feh")],
+            FileType::Program(program, _) => program.get_suffixes(),
+        }
+    }
 
-	fn grade(self, file_name: &str, dir_name: &str, prog_name: &str) -> Option<u32> {
-		if !Path::new(file_name).exists() {
-			return None
-		}
+    fn grade(self, file_name: &str, dir_name: &str, prog_name: &str) -> Option<u32> {
+        if !Path::new(file_name).exists() {
+            return None;
+        }
 
-		match self {
-			FileType::DOC => {
-				Command::new(prog_name).arg(file_name).stdin(Stdio::null()).stdout(Stdio::null()).spawn().unwrap();
-			}
-			FileType::PIC => {
-				Command::new(prog_name).arg(file_name).stdin(Stdio::null()).stdout(Stdio::null()).spawn().unwrap();
-			}
-			FileType::PROGRAM(program, is_raw) => {
-				if is_raw {
-					Command::new("kate").stdin(Stdio::null()).stdout(Stdio::null()).arg(file_name).spawn().unwrap();
-				} else {
-					return program.grade(file_name, dir_name, prog_name);
-				}
-			}
-		}
+        match self {
+            FileType::Doc => {
+                Command::new(prog_name)
+                    .arg(file_name)
+                    .stdin(Stdio::null())
+                    .stdout(Stdio::null())
+                    .spawn()
+                    .unwrap();
+            }
+            FileType::Pic => {
+                Command::new(prog_name)
+                    .arg(file_name)
+                    .stdin(Stdio::null())
+                    .stdout(Stdio::null())
+                    .spawn()
+                    .unwrap();
+            }
+            FileType::Program(program, is_raw) => {
+                if is_raw {
+                    Command::new("kate")
+                        .stdin(Stdio::null())
+                        .stdout(Stdio::null())
+                        .arg(file_name)
+                        .spawn()
+                        .unwrap();
+                } else {
+                    return program.grade(file_name, dir_name, prog_name);
+                }
+            }
+        }
 
-		println!("Type the score(0-100):");
-		// Read the score
-		let mut line = String::new();
-		io::stdin().read_line(&mut line).unwrap();
-		Some(line.trim().parse().unwrap_or(0))
-	}
-
+        println!("Type the score(0-100):");
+        // Read the score
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
+        Some(line.trim().parse().unwrap_or(0))
+    }
 }
-
 
 // I use system in the rust code. To prevent any security issue, I decide to allow only string literal as problem_name
 #[derive(Clone)]
 pub struct Problem {
-	problem_name: &'static str,
-	file_type: FileType,
-	pub percentage: u32,
+    problem_name: &'static str,
+    file_type: FileType,
+    pub percentage: u32,
 }
 
-
 impl Problem {
-	pub fn new(problem_name: &'static str, file_type: FileType, percentage: u32) -> Self {
-		Self {
-			problem_name,
-			file_type,
-			percentage
-		}
-	}
+    pub fn new(problem_name: &'static str, file_type: FileType, percentage: u32) -> Self {
+        Self {
+            problem_name,
+            file_type,
+            percentage,
+        }
+    }
 
-	// Per problem grade
-	pub fn grade(&self, dir: &str) -> u32 {
-		// Find possible suffixes
-		let suffixes = self.file_type.get_suffixes();
+    // Per problem grade
+    pub fn grade(&self, dir: &str) -> u32 {
+        // Find possible suffixes
+        let suffixes = self.file_type.get_suffixes();
 
-		for suffix in suffixes {
-			// Get true file name and search each
-			println!("Searching {}.{}", self.problem_name, suffix.0);
-			let file_name = format!("{dir}/{}.{}", self.problem_name, suffix.0);
+        for item in suffixes {
+            let grading_inner = |suffix| {
+                println!("Searching {}.{}", self.problem_name, suffix);
+                let file_name = format!("{dir}/{}.{}", self.problem_name, suffix);
+                self.file_type.grade(&file_name, dir, item.1)
+            };
 
-			if let Some(score) = self.file_type.grade(&file_name, &dir, suffix.1) {
-				return score
-			}
-		}
-		
-		101
-	}
+            if let Some(grade) = grading_inner(item.0.to_uppercase()) {
+                return grade;
+            } else if let Some(grade) = grading_inner(item.0.to_lowercase()) {
+                return grade;
+            }
+            // Get true file name and search each
+        }
+
+        101
+    }
 }
 
 pub struct Assignment {
-	problems: Vec<Problem>,
+    problems: Vec<Problem>,
 }
 
 impl Assignment {
-	pub fn new() -> Self {
-		Self {
-			problems: vec![]
-		}
-	}
-	pub fn new_with_problems(problems: &[Problem]) -> Self {
-		let mut res = Self {
-			problems: vec![]
-		};
+    pub fn new() -> Self {
+        Self { problems: vec![] }
+    }
 
-		for problem in problems {
-			res.problems.push(problem.clone());
-		}
+    pub fn new_with_problems(problems: &[Problem]) -> Self {
+        let mut res = Self { problems: vec![] };
 
-		res
-	}
+        for problem in problems {
+            res.problems.push(problem.clone());
+        }
 
-	pub fn add_entry(&mut self, problem: Problem) {
-		self.problems.push(problem);
-	}
+        res
+    }
 
-	pub fn get_comment() -> String {
-		let mut line = String::new();
-		io::stdin().read_line(&mut line).unwrap();
-		line = line.trim().to_string();
-		line
-	}
+    pub fn add_entry(&mut self, problem: Problem) {
+        self.problems.push(problem);
+    }
 
-	// Read from dir, get the (score, comment) of an assignment
-	pub fn grade(&self, dir_name: &str, student_id: &str) -> (f64, String) {
-		let (mut scores, mut total_percentage) = (0.0, 0.0);
-		let mut comment = String::new();
+    pub fn get_comment() -> String {
+        let mut line = String::new();
+        io::stdin().read_line(&mut line).unwrap();
+        line = line.trim().to_string();
+        line
+    }
 
-		let mut dir_name = dir_name.to_string();
+    // Read from dir, get the (score, comment) of an assignment
+    pub fn grade(&self, dir_name: &str, student_id: &str) -> (f64, String) {
+        let (mut scores, mut total_percentage) = (0.0, 0.0);
+        let mut comment = String::new();
 
-		// Get inner directory
-		if Path::new(&format!("{}/{}", dir_name, student_id.to_uppercase())).exists() {
-			dir_name += "/";
-			dir_name += &student_id.to_uppercase();
-		} else if Path::new(&format!("{}/{}", dir_name, student_id.to_lowercase())).exists() {
-			dir_name += "/";
-			dir_name += &student_id.to_lowercase();
-		}
+        let mut dir_name = PathBuf::from(dir_name);
 
-		let mut need_reviews = false;
-		// Grade all problems
-		for problem in &self.problems {
-			// Grade scores
-			println!("Grading problem {} on student: {student_id}", problem.problem_name);
-			
-			let score = problem.grade(&dir_name);
-			if score != 101 {
-				scores += score as f64 * problem.percentage as f64;
-			} else {
-				need_reviews = true;
-			}
+        // Get inner directory
+        dir_name.push(student_id.to_uppercase());
+        if !dir_name.exists() {
+            dir_name.pop();
+            dir_name.push(student_id.to_lowercase());
+            if !dir_name.exists() {
+                dir_name.pop();
+            }
+        }
 
+        let mut need_reviews = false;
+        // Grade all problems
+        for problem in &self.problems {
+            // Grade scores
+            println!(
+                "Grading problem {} on student: {student_id}",
+                problem.problem_name
+            );
 
-			// Get comment
-			println!("Write down your comment:");
-			let cur_comment = Self::get_comment();
-			if cur_comment.len() != 0 {
-				comment +=  &format!("{}: {}.", problem.problem_name, cur_comment);
-			}
+            let score = problem.grade(dir_name.to_str().unwrap());
+            if score != 101 {
+                scores += score as f64 * problem.percentage as f64;
+            } else {
+                need_reviews = true;
+            }
 
-			total_percentage += problem.percentage as f64;
-		}
+            // Get comment
+            println!("Write down your comment:");
+            let cur_comment = Self::get_comment();
+            if !cur_comment.is_empty() {
+                comment += &format!("{}: {}.", problem.problem_name, cur_comment);
+            }
 
-		(scores / total_percentage, comment + if need_reviews {"Need manual review"} else {""})
-	}
+            total_percentage += problem.percentage as f64;
+        }
+
+        (
+            scores / total_percentage,
+            comment
+                + if need_reviews {
+                    "Need manual review"
+                } else {
+                    ""
+                },
+        )
+    }
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,145 +1,188 @@
-use std::{result, collections::HashSet, fs::{self, File, OpenOptions}, io::{self, Write, Read, BufReader, BufRead}, path::{Path, PathBuf}, process::Stdio};
+use std::{
+    collections::HashSet,
+    fs::{self, File, OpenOptions},
+    io::{self, BufRead, BufReader, Read, Write},
+    path::{Path, PathBuf},
+    process::Stdio,
+    result,
+};
 
-use assignment::{Assignment, Problem, ProgramType, FileType};
-use calamine::{open_workbook_auto, Error, Reader, RangeDeserializer, RangeDeserializerBuilder, Xlsx, open_workbook};
-use fuzzy_matcher::{skim::SkimMatcherV2, FuzzyMatcher};
-use serde::{Deserialize, Serialize, de::value::BytesDeserializer};
+use assignment::{Assignment, FileType, Problem, ProgramType};
+use calamine::{
+    open_workbook, open_workbook_auto, Error, RangeDeserializer, RangeDeserializerBuilder, Reader,
+    Xlsx,
+};
 use clap::Parser;
+use fuzzy_matcher::{skim::SkimMatcherV2, FuzzyMatcher};
+use serde::{de::value::BytesDeserializer, Deserialize, Serialize};
 
 mod assignment;
 
 #[derive(Deserialize, Serialize, Debug)]
 struct Record {
-	#[serde(rename="序號(No.)")]
-	id: u32, // class generated ID
-	#[serde(rename="學號(Stu No.)")]
-	student_number: String, // student number
+    #[serde(rename = "序號(No.)")]
+    id: u32, // class generated ID
+    #[serde(rename = "學號(Stu No.)")]
+    student_number: String, // student number
 }
 
 #[derive(Parser, Debug)]
 struct Args {
-	#[clap(short, long, default_value = "./")]
-	working_dir: String,
-	#[clap(short, long, default_value = "test.xlsx")]
-	file_path: String,
-	#[clap(short, long, default_value = "output.txt")]
-	output_file: String,
+    #[clap(short, long, default_value = "./")]
+    working_dir: String,
+    #[clap(short, long, default_value = "test.xlsx")]
+    file_path: String,
+    #[clap(short, long, default_value = "output.txt")]
+    output_file: String,
 }
 
-
 fn list_directory(path: &str) -> Vec<String> {
-	let files = fs::read_dir(path).unwrap();
-	let mut res = vec![];
-	
-	for file in files {
-		res.push(file.unwrap().path().to_str().unwrap().to_string());
-	}
+    let files = fs::read_dir(path).unwrap();
+    let mut res = vec![];
 
-	res
+    for file in files {
+        res.push(file.unwrap().path().to_str().unwrap().to_string());
+    }
+
+    res
 }
 
 fn file_matching(matcher: &SkimMatcherV2, file_name: &str, items: &Vec<String>) -> Option<String> {
-	let mut hightest_score = 0;
-	let mut res = String::new();
+    let mut hightest_score = 0;
+    let mut res = String::new();
 
-	for item in items {
-		if let Some(score) = matcher.fuzzy_match(&item, &file_name) {
-			if hightest_score < score {
-				hightest_score = score;
-				res = item.clone();
-			}
-		}
-	}
+    for item in items {
+        if let Some(score) = matcher.fuzzy_match(&item, &file_name) {
+            if hightest_score < score {
+                hightest_score = score;
+                res = item.clone();
+            }
+        }
+    }
 
-	if res.is_empty() {
-		None
-	} else {
-		Some(res)
-	}
+    if res.is_empty() {
+        None
+    } else {
+        Some(res)
+    }
 }
 
-fn grading(item: & Record, matcher: &SkimMatcherV2, dirs: &Vec<String>, assignment: &Assignment, output_file: &mut File) {
-	let mut file_name = PathBuf::new();
+fn grading(
+    item: &Record,
+    matcher: &SkimMatcherV2,
+    dirs: &Vec<String>,
+    assignment: &Assignment,
+    output_file: &mut File,
+) {
+    let mut file_name = PathBuf::new();
 
-	println!("Start grading student: {}", &item.student_number);
-	
-	// Match with zip file in file_directory
-	if let Some(matched_directory) = file_matching(matcher, &item.student_number, dirs) {
-		file_name.push(matched_directory);
-		file_name.push(item.student_number.to_uppercase());
-		file_name.set_extension("zip");
+    println!("Start grading student: {}", &item.student_number);
 
-		// Use lowercase
-		if !file_name.exists() {
-			file_name.pop();
-			file_name.push(item.student_number.to_lowercase());
-			file_name.set_extension("zip");
-		}
-	} else {
-		println!("Zip file for {} not found. Please type a file name for your zip file:", item.student_number);
-		let mut buf = String::new();
-		io::stdin().read_line(&mut buf).unwrap();
-		file_name = PathBuf::from(buf);
-	}
+    // Match with zip file in file_directory
+    if let Some(matched_directory) = file_matching(matcher, &item.student_number, dirs) {
+        file_name.push(matched_directory);
+        file_name.push(item.student_number.to_uppercase());
+        file_name.set_extension("zip");
 
-	let (scores, comment) = if file_name.exists() {
-		// Unzip the file
-		let workdir = file_name.parent().unwrap().to_str().unwrap();
-		println!("Unziping zipfile: {:?} in workdir: {workdir}", file_name);
-		std::process::Command::new("unzip").arg(&file_name).arg(&format!("-d{workdir}")).stdout(Stdio::null()).status().unwrap();
+        // Use lowercase
+        if !file_name.exists() {
+            file_name.pop();
+            file_name.push(item.student_number.to_lowercase());
+            file_name.set_extension("zip");
+        }
+    } else {
+        println!(
+            "Zip file for {} not found. Please type a file name for your zip file:",
+            item.student_number
+        );
+        let mut buf = String::new();
+        io::stdin().read_line(&mut buf).unwrap();
+        file_name = PathBuf::from(buf);
+    }
 
-		// Grade the scores
-		assignment.grade(workdir, &item.student_number)
-	} else {
-		(-1.0, "Need manual review".to_string())
-	};
+    let (scores, comment) = if file_name.exists() {
+        // Unzip the file
+        let workdir = file_name.parent().unwrap().to_str().unwrap();
+        println!("Unziping zipfile: {:?} in workdir: {workdir}", file_name);
+        std::process::Command::new("unzip")
+            .arg(&file_name)
+            .arg(&format!("-d{workdir}"))
+            .stdout(Stdio::null())
+            .status()
+            .unwrap();
 
-	output_file.write_fmt(format_args!("{scores}\t{comment}\n")).unwrap();
-	output_file.flush().unwrap();
+        // Grade the scores
+        assignment.grade(workdir, &item.student_number)
+    } else {
+        (-1.0, "Need manual review".to_string())
+    };
+
+    output_file
+        .write_fmt(format_args!("{scores}\t{comment}\n"))
+        .unwrap();
+    output_file.flush().unwrap();
 }
 
 fn count_line(f: File) -> (File, u32) {
-	let mut cur = 0;
-	let mut reader = BufReader::new(f);
-	let mut line = String::new();
+    let mut cur = 0;
+    let mut reader = BufReader::new(f);
+    let mut line = String::new();
 
-	while let Ok(byte) = reader.read_line(&mut line) {
-		if byte == 0 {
-			break;
-		}
+    while let Ok(byte) = reader.read_line(&mut line) {
+        if byte == 0 {
+            break;
+        }
 
-		cur += 1;
-	}
+        cur += 1;
+    }
 
-	(reader.into_inner(), cur)
+    (reader.into_inner(), cur)
 }
 
 fn main() -> Result<(), Error> {
-	let args = Args::parse();
+    let args = Args::parse();
 
-	let dirs = list_directory(&args.working_dir);
+    let dirs = list_directory(&args.working_dir);
 
-	let mut workbook = open_workbook_auto(args.file_path)?;
-	let worksheet = workbook.worksheet_range("成績")?;
+    let mut workbook = open_workbook_auto(args.file_path)?;
+    let worksheet = workbook.worksheet_range("成績")?;
 
-	let mut iterator = RangeDeserializerBuilder::new().from_range(&worksheet)?;
-	let matcher = SkimMatcherV2::default();
+    let iterator = RangeDeserializerBuilder::new().from_range(&worksheet)?;
+    let matcher = SkimMatcherV2::default();
 
-	// TODO: Design your own assignment
-	let mut assignment = Assignment::new();
-	assignment.add_entry(Problem::new("1", FileType::PIC, 20));
-	assignment.add_entry(Problem::new("1", FileType::DOC, 20));
-	assignment.add_entry(Problem::new("2", FileType::DOC, 30));
-	assignment.add_entry(Problem::new("3", FileType::PROGRAM(ProgramType::C, true), 15));
-	assignment.add_entry(Problem::new("3", FileType::PIC, 15));
+    // TODO: Design your own assignment
+    let mut assignment = Assignment::new();
+    assignment.add_entry(Problem::new("1", FileType::Pic, 20));
+    assignment.add_entry(Problem::new("1", FileType::Doc, 20));
+    assignment.add_entry(Problem::new("2", FileType::Doc, 30));
+    assignment.add_entry(Problem::new(
+        "3",
+        FileType::Program(ProgramType::C, true),
+        15,
+    ));
+    assignment.add_entry(Problem::new("3", FileType::Pic, 15));
 
-	let output_file = OpenOptions::new().read(true).write(true).create(true).open(&args.output_file).unwrap();
+    let output_file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .truncate(true)
+        .open(&args.output_file)
+        .unwrap();
 
-	let (mut output_file, lines) = count_line(output_file);
+    let (mut output_file, lines) = count_line(output_file);
 
-	println!("{lines} lines previously.");
-	
-	iterator.skip(lines as usize).for_each(move |item| grading(&item.unwrap(), &matcher, &dirs, &assignment, &mut output_file));
+    println!("{lines} lines previously.");
 
-	Ok(())
+    iterator.skip(lines as usize).for_each(move |item| {
+        grading(
+            &item.unwrap(),
+            &matcher,
+            &dirs,
+            &assignment,
+            &mut output_file,
+        )
+    });
+
+    Ok(())
 }
